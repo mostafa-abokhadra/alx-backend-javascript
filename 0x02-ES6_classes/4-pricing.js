56ec8efd0382d607673484a8e798fcba46baf8be
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-26 00:49:01 +0300

    5

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from './3-currency'
+
+export default class Pricing {
+    constructor(amount, currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+    set amount(amount){
+        if (typeof amount !== 'number'){
+            throw new TypeError("Amount must be a number");
+        }
+        this._amount = amount;
+    }
+    get amount(){
+        return this._amount;
+    }
+    set currency(currency){
+        if (!currency instanceof Currency){
+            throw new TypeError("Currency must be a currency");
+        }
+        this._currency = currency;
+    }
+    get currency(){
+        return this._currency;
+    }
+    displayFullPrice() {
+        return `${this._amount} ${this._currency._name} (${this._currency._code})`
+    }
+    static convertPrice(amount, conversionRate){
+        if (typeof amount !== 'number' || typeof conversionRate !== 'number'){
+            throw new TypeError("both args must be a number")
+        }
+        return amount * conversionRate;
+    }
+}
+
+
