01ea5f057cf6168ebc3149fa1f6bd42349e89bb3
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-26 14:49:59 +0300

    4

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import uploadPhoto, { createUser } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((val) => {
+  return uploadPhoto().then((val) => {
     process.stdout.write(`${val.body} `);
     createUser().then((val) => {
       console.log(`${val.firstName} ${val.lastName}`);
@@ -12,3 +12,4 @@ export default function handleProfileSignup() {
     console.log('Signup system offline');
   });
 }
+
