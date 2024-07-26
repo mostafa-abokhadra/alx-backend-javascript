0d8c6fab6d626ed689edcdd5eb38b22215c35334
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-26 19:27:35 +0300

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,3 +1,17 @@
-export default function asyncUploadUser() {
-    
+import {uploadPhoto, createUser } from'./utils'
+
+export default async function asyncUploadUser() {
+    try {
+        let res1 = await uploadPhoto();
+        let res2 = await createUser();
+        return {
+            photo: res1,
+            user: res2
+        };
+    } catch(err) {
+        return {
+            photo: null,
+            user: null,
+          };
+    }
 }
