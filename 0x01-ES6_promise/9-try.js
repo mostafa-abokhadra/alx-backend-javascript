37a3126b3e558a446dbce5b07d414a927ff17a40
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-26 17:51:10 +0300

    10

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,12 @@
 export default function guardrail(mathFunction) {
-    
+  const queue = [];
+  try {
+    const val = mathFunction();
+    queue.push(val);
+  } catch (err) {
+    queue.push(err);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
