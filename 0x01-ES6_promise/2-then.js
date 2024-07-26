d398a259b5d3f579178fb5b9b00844544f18606b
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-26 14:14:48 +0300

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const aPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(Error());
-    }
-  }).then(() => {
+  promise.then(() => {
     console.log('Got a response from the API');
-  });
-
-  return aPromise;
+    return ({ status: 200, body: 'success' });
+  }, () => (Error()));
 }
