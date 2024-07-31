6f933ad10cceafeea45cf1583f9462fde8fbb224
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-31 03:22:03 +0300

    trying to optimize 5

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,16 +3,11 @@ export default function updateStudentGradeByCity(arr, city, newGrades) {
   const graded = newGrades.map((gradeObj) => {
     const studentToAddTheGrade = filtered.find((current) => current.id === gradeObj.studentId);
     studentToAddTheGrade.grade = gradeObj.grade;
-    // if (Object.prototype.hasOwnProperty.call(gradeObj, 'grade')) {
-    //   studentToAddTheGrade.grade = gradeObj.grade;
-    // } else {
-    //   studentToAddTheGrade.grade = 'N/A';
-    // }
     return studentToAddTheGrade;
   });
   return filtered.map((obj) => {
     if (graded.find((current) => current.id === obj.id) === undefined) {
-      // eslint-disable-next-line no-param-reassign
+    // eslint-disable-next-line no-param-reassign
       obj.grade = 'N/A';
     }
     return obj;
