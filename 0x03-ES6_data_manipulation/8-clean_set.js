2d3bd5ae34fc55c39adca62f6b221fe58c302e13
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-31 04:42:18 +0300

    task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   const output = [];
-  if (startString.length === 0 || typeof startString !== 'string') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   set.forEach((elem) => {
-    if (typeof elem === 'string' && elem.slice(0, startString.length) === startString) {
-      output.push(elem.slice(startString.length));
+    if (typeof elem === 'string' && elem.startsWith(startString)) {
+      output.push(s.slice(startString.length));
     }
   });
   return output.join('-');
