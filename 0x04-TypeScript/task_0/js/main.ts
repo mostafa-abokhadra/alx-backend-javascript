6f7520178e48c8e02cba92b2c7a918d1f314ad6e
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 00:54:37 +0300

    task 1 done

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,11 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+let studentOne: Student;
+let studentTwo: Student;
+
+let studentsList: Student[] = [studentOne, studentTwo];
