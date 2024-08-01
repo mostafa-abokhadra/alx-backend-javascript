6a726284bfd997a1d2c234437c685e3a118bd41b
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-01 03:54:12 +0300

    task 6

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,43 @@
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string
+}
+interface TeacherInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workTeacherTasks(): string
+}
+class Director implements DirectorInterface {
+    constructor(){}
+    workFromHome(): string {
+        return `Working from home`;
+    }
+    getToWork(): string {
+        return `Getting a coffee break`;
+    }
+    workDirectorTasks(): string {
+        return `Getting to director tasks`
+    }
+    getCoffeeBreak(): string{
+        return `Getting a coffee break`;
+    }
+}
+class Teacher  implements TeacherInterface {
+    constructor(){}
+    workFromHome(): string {
+        return `Cannot work from home`;
+    }
+    workTeacherTasks(): string {
+        return `Getting to work`
+    }
+    getCoffeeBreak(): string{
+        return `Cannot have a break`;
+    }
+}
+function createEmployee (salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
